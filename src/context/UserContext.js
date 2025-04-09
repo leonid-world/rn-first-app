af6e02d425abe36fe1cf704b2891d6e76c937aaa
leonid <leonid.always@gmail.com>
2025-04-09 09:07:40 +0900

    lint 적용

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,29 +1,29 @@
-import React, {createContext, useState, useEffect } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 import { getUser } from "../utils/authStorage";
 
 export const UserContext = createContext();
 
-export function UserProvider({children}){
-    const [user, setUser] = useState(null);
+export function UserProvider({ children }) {
+  const [user, setUser] = useState(null);
 
-    const [isLoading, setIsLoading] = useState(true); //로딩 상태 추가
+  const [isLoading, setIsLoading] = useState(true); //로딩 상태 추가
 
-    useEffect(() => {
-        const init = async () => {
-            const storedUser = await getUser();
-            if (storedUser){
-                setUser(storedUser); //자동 로그인
-            }
-            setIsLoading(false);
-        }
+  useEffect(() => {
+    const init = async () => {
+      const storedUser = await getUser();
+      if (storedUser) {
+        setUser(storedUser); //자동 로그인
+      }
+      setIsLoading(false);
+    };
 
-        init();
-    }, [])
+    init();
+  }, []);
 
-    return (
-        <UserContext.Provider value={{user, setUser, isLoading}}>
-            {children}
-        </UserContext.Provider>
-    )
+  return (
+    <UserContext.Provider value={{ user, setUser, isLoading }}>
+      {children}
+    </UserContext.Provider>
+  );
 }
